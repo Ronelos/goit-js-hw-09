1640a7febd8e5704ed20e89cf103f514b60fd0bf
Andrii Kryvyi <87859325+Ronelos@users.noreply.github.com>
2022-01-13 23:16:03 +0200

    done first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000) 
+  start.disabled = true; 
+  stop.disabled = false;
+}
+
+start.addEventListener('click', changeColor) 
+stop.addEventListener('click', stopChange) 
+
+function stopChange() {
+clearInterval(timerId);
+start.disabled = false; 
+stop.disabled = true; 
+}
+
 
